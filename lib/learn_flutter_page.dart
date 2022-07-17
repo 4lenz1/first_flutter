7dc0f518f38fc1e371e8f041a3f5a06b51b9a4e1
4lenz1 <pingfallenzone@gmail.com>
2022-07-18 00:53:41 +0900

    add learn flutter page

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -8,6 +8,8 @@ class LearnPage extends StatefulWidget {
 }
 
 class _LearnPageState extends State<LearnPage> {
+  bool isSwitch = false;
+  bool? isCheck = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,68 +22,100 @@ class _LearnPageState extends State<LearnPage> {
           },
           icon: const Icon(Icons.arrow_back_ios),
         ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              debugPrint('actions');
+            },
+            icon: const Icon(
+              Icons.info_outline,
+            ),
+          )
+        ],
       ),
-      body: Column(children: [
-        Image.asset('images/test.jpeg'),
-        const SizedBox(
-          height: 10,
-        ),
-        const Divider(
-          color: Colors.black,
-        ),
-        Container(
-          margin: const EdgeInsets.all(10.0),
-          padding: const EdgeInsets.all(10),
-          color: Colors.blueGrey,
-          width: double.infinity,
-          child: const Center(
-            child: Text(
-              'XDDDD',
-              style: TextStyle(
-                color: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Image.asset('images/test.jpeg'),
+          const SizedBox(
+            height: 10,
+          ),
+          const Divider(
+            color: Colors.black,
+          ),
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10),
+            color: Colors.blueGrey,
+            width: double.infinity,
+            child: const Center(
+              child: Text(
+                'XDDDD',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
               ),
             ),
           ),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            debugPrint('elevate button');
-          },
-          child: const Text('Elevated Button'),
-        ),
-        OutlinedButton(
-          onPressed: () {
-            debugPrint('outline button');
-          },
-          child: const Text('outline Button'),
-        ),
-        TextButton(
-          onPressed: () {
-            debugPrint('text button');
-          },
-          child: const Text('text Button'),
-        ),
-        GestureDetector(
-          behavior: HitTestBehavior.opaque,
-          onTap: () {
-            debugPrint('ededededed');
-          },
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: const [
-              Icon(
-                Icons.local_fire_department,
-                color: Colors.blue,
-              ),
-              Text('ROW Widget'),
-              Icon(
-                color: Colors.blue,
-                Icons.local_fire_department,
-              ),
-            ],
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: isSwitch ? Colors.green : Colors.blue,
+            ),
+            onPressed: () {
+              debugPrint('elevate button');
+            },
+            child: const Text('Elevated Button'),
           ),
-        )
-      ]),
+          OutlinedButton(
+            onPressed: () {
+              debugPrint('outline button');
+            },
+            child: const Text('outline Button'),
+          ),
+          TextButton(
+            onPressed: () {
+              debugPrint('text button');
+            },
+            child: const Text('text Button'),
+          ),
+          GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            onTap: () {
+              debugPrint('ededededed');
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [
+                Icon(
+                  Icons.local_fire_department,
+                  color: Colors.blue,
+                ),
+                Text('ROW Widget'),
+                Icon(
+                  color: Colors.blue,
+                  Icons.local_fire_department,
+                ),
+              ],
+            ),
+          ),
+          Switch(
+              value: isSwitch,
+              onChanged: (bool newValue) {
+                setState(() {
+                  isSwitch = newValue;
+                });
+              }),
+          CheckboxListTile(
+              title: const Text('WAWAAAA'),
+              value: isCheck,
+              onChanged: (bool? newValue) {
+                setState(() {
+                  isCheck = newValue;
+                });
+              }),
+          Image.network(
+              'https://dart.dev/assets/shared/dart-logo-for-shares.png')
+        ]),
+      ),
     );
   }
 }
